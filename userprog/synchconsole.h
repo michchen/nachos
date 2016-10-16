1fe0234219f7eff7e225bb486fed52f7804c30b2
Michelle  Chen <mchen@tx.cs.wm.edu>
2016-10-16 04:02:57 -0400

    Starting synchconsole

---
+++ userprog/synchconsole.h
@@ -0,0 +1,27 @@
+#ifndef SYNCHCONSCOLE_H
+#define SYNCHCONSCOLE_H
+
+#include "console.h"
+#include "synch.h"
+
+class SynchConsole{
+public:
+	SynchConsole(char *file);
+
+	~SynchConsole();
+
+	void ReadFile();
+
+	void WriteFile();
+
+	void ReadDone();
+
+	void WriteDone();
+
+private:
+	Semaphore *readAvail;
+	Semaphore *writeDone;
+	Console *console;
+};
+
+#endif //SYNCHCONSCOLE_H
