d549d6d1a2268a98d21bb41bf3f00cc1edd7991a
Michelle Chen <micheniverse@gmail.com>
2016-10-28 11:48:00 -0400

    translating memory?

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -98,7 +98,6 @@ AddrSpace::AddrSpace(OpenFile *executable)
 	pageTable[i].readOnly = false;  // if the code segment was entirely on 
 					// a separate page, we could set its 
 					// pages to be read-only
-	fprintf(stderr, "%s\n","ignored?" );
     }
 #endif    
 
@@ -109,20 +108,31 @@ AddrSpace::AddrSpace(OpenFile *executable)
         bzero(&(machine->mainMemory[pageTable[i].physicalPage * PageSize]), PageSize);
     }
 
+    int virtaddr;
+    int addrtrans;
 // then, copy in the code and data segments into memory
     if (noffH.code.size > 0) {
         DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
 			noffH.code.virtualAddr, noffH.code.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr]),
-			noffH.code.size, noffH.code.inFileAddr);
+        virtaddr = noffH.code.virtualAddr;
+        while (virtaddr < (noffH.code.size + noffH.code.virtualAddr)) {
+            addrtrans = AddrTranslation(virtaddr);
+            executable->ReadAt(&(machine->mainMemory[addrtrans]),
+			     1, noffH.code.inFileAddr + (virtaddr - noffH.code.virtualAddr));
+            virtaddr++;
+        }
     }
     if (noffH.initData.size > 0) {
         DEBUG('a', "Initializing data segment, at 0x%x, size %d\n", 
 			noffH.initData.virtualAddr, noffH.initData.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr]),
-			noffH.initData.size, noffH.initData.inFileAddr);
+        virtaddr = noffH.code.virtualAddr;
+        while (virtaddr < (noffH.initData.size + noffH.initData.virtualAddr)) {
+            addrtrans = AddrTranslation(virtaddr);
+            executable->ReadAt(&(machine->mainMemory[addrtrans]),
+			1, noffH.initData.inFileAddr + (virtaddr-noffH.initData.virtualAddr));
+            virtaddr++;
+        }
     }
-    fprintf(stderr, "%s\n", "what");
 
 }
 
