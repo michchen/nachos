4fa00a0b55ef34cd4c503dca78d950722fdf585c
Michelle  Chen <mchen@il.cs.wm.edu>
2016-11-03 21:51:29 -0400

    changing some .c files

--- test/shell.c
+++ test/shell.c
@@ -25,15 +25,36 @@ main()
 
 	} while( buffer[i++] != '\n' );
 
+
 	buffer[--i] = '\0';
+
+	prints(buffer, ConsoleOutput);
+	prints("\n", ConsoleOutput);
 
 	args[0] = (char *)0;
 
 	if( i > 0 ) {
 	  newProc = Fork();
-          if (newProc == 0) Exec(buffer, args);
+          if (newProc == 0) {
+          	prints("doing kid stuff");
+          	Exec(buffer, args);
+          }
 	  else Join(newProc);
 	}
     }
+}
+
+prints(s,file)
+char *s;
+OpenFileId file;
+
+{
+  int count = 0;
+  char *p;
+
+  p = s;
+  while (*p++ != '\0') count++;
+  Write(s, count, file);  
+
 }
 
