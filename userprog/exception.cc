565f6f3917a99607568820537a990413fdf343a8
Michelle  Chen <mchen@il.cs.wm.edu>
2016-11-04 03:21:26 -0400

    fixed some errors with exec

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -547,9 +547,12 @@ void sysCallExec(){
     argvAddr[0] = sp;
 
 
+    fprintf(stderr, "%s %s\n","the argv[0] val", argv[0] );
+    fprintf(stderr, "And now the filename %s\n", fileName);
     int initialVal = 0;
-    if (argv[0] == fileName) {
+    if (!strcmp(argv[0], fileName)) {
       initialVal = 1;
+      fprintf(stderr, "%s\n", "we've found a filename match!");
     }
     for (i=initialVal; i<argc; i++) {
       if (argv[i] != fileName) {
@@ -557,29 +560,40 @@ void sysCallExec(){
         sp -= len;
         for (int j = 0; j < len; j++){
           machine->mainMemory[currentThread->space->AddrTranslation(sp+j)] = argv[i][j];
+          //fprintf(stderr, "We've read this char into memory %c\n",argv[i][j] );
         }
         if (initialVal == 0)
           argvAddr[i+1] = sp;
         else
           argvAddr[i] = sp;
-        sp = sp & ~3;
+        
+        // Jose looks to do this once?
       }
+
+      fprintf(stderr, "Reading the data into the memory %s\n", argv[i]);
     }
 
+    sp = sp & ~3;
+    
     if (initialVal == 0) {
       argc++;
     }
 
-    sp -= sizeof(int) *2;
+    sp -= sizeof(int) *4;
 
     for(i = 0; i<argc; i++) {
       *(unsigned int *)&machine->mainMemory[currentThread->space->AddrTranslation((sp+i*4))] = (unsigned int) argvAddr[i];
     }
     DEBUG('a',"About to Write\n");
     machine->WriteRegister(4, argc);
+    printf("%d\n",argc );
+    fprintf(stderr, "%s\n", "did we write one");
     machine->WriteRegister(5, sp);
-
+    fprintf(stderr, "%s\n", "what about this one");
     machine->WriteRegister(StackReg, sp-8);
+    fprintf(stderr, "%s\n", "last one");
+
+    // machine->Run();
 
 
     // //Create a new Thread
